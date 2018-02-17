514f694ed0363abc7d28485f2125fbc504e15a5a
Pierre-Xavier Rousseau <pierre.xavier@gmail.com>
2018-02-17 17:36:05 +0100

    resolve conflict

--- lib/omnifocus_parser/node.rb
+++ lib/omnifocus_parser/node.rb
@@ -2,14 +2,6 @@ class Node
    attr_accessor :text, :comment, :node_id
 
    def initialize(node_id, text=nil, comment=nil)
-<<<<<<< HEAD
       @node_id = node_id
    end
-
-=======
-
-   end
-
-
->>>>>>> c8861d0f554567e1c6878bd5edd506f2d9806800
 end
